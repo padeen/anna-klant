4599bb4ad80264ac6f0b1815878f098e0f921317
Paulo Deen <paulo.deen@gmail.com>
2024-02-09 13:51:43 +0100

    Update servicelines

--- src/lib/serviceLines.ts
+++ src/lib/serviceLines.ts
@@ -1,9 +1,12 @@
 export const serviceLines = [
   'Hello. Thank you for contacting me.',
+  'Sorry I haven\'t replied to you earlier. We were too busy',
+  'Yes, it\'s me on the photo. I took it at the beach last summer during my free time.',
   'What is your question?',
-  'I do relax massage and sports massage.',
+  'We do relax massage and sports massage.',
   'You need to wear a towel.',
   'No erotic massage.',
+  'Right now we only have half hour massage and one hour massage.',
   'The price for one hour is 50 euro.',
   'The price for a half hour is 30 euro.',
   'The opening times are everyday from 10:30 till 21:30.',
